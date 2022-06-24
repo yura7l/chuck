59e71f73a056240ba06a17f3df76a336add62cb8
Yurii <yura93lobur@gmail.com>
2022-06-24 17:37:59 +0300

    Categories from API

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import 'bootstrap';
 
 // import template styles
 import './scss/main.scss'
+
+import Categories from './js/components/categories'
+const categories = new Categories()
+categories.render()
---
+++ src/js/components/categories.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+class Categories {
+    async render() {
+        try {
+            const categories = (await axios.get(`https://api.chucknorris.io/jokes/categories`)).data
+
+            if (categories) {
+                const parent = document.querySelector('.categories')
+                categories.forEach(cat => {
+                    let input = document.createElement('input')
+                    input.type = 'radio'
+                    input.id = cat
+                    input.name = 'categories'
+                    input.value = cat
+                    parent.appendChild(input)
+
+                    let label = document.createElement('label')
+                    label.setAttribute('for', cat)
+                    label.innerHTML = cat
+                    label.classList.add('categories__item')
+                    parent.appendChild(label)
+                })
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export default Categories
